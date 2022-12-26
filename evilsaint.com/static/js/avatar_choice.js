ad424c6aee2af9cbfc038e1343f31250ddfbb0d8
Azure DevBox <devopme@evilsiant.me>
2022-12-26 18:56:28 +0000

    updated registry page

--- evilsaint.com/static/js/avatar_choice.js
+++
@@ -1,8 +0,0 @@
-$( "#avatarModal .btn" ).click(
-    function() {
-        $( "#avatar-choice, #my-avatar" ).attr(
-            "src",
-            $('input:checked').css( "background-image" ).slice( 5, -2 )
-        );
-    }
-);
